a8aef2fbeb65e2e47475ca9c3fcb1780d1e4b995
Bryan <bryanmauriciopalma@gmail.com>
2020-04-21 12:52:25 -0600

    First Commit

---
+++ Estadistica/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Estadistica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopMenu = new System.Windows.Forms.Panel();
+            this.SideBar = new System.Windows.Forms.Panel();
+            this.Content = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // TopMenu
+            // 
+            this.TopMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopMenu.Location = new System.Drawing.Point(0, 0);
+            this.TopMenu.Name = "TopMenu";
+            this.TopMenu.Size = new System.Drawing.Size(1400, 100);
+            this.TopMenu.TabIndex = 0;
+            // 
+            // SideBar
+            // 
+            this.SideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SideBar.Location = new System.Drawing.Point(0, 100);
+            this.SideBar.Name = "SideBar";
+            this.SideBar.Size = new System.Drawing.Size(200, 800);
+            this.SideBar.TabIndex = 1;
+            // 
+            // Content
+            // 
+            this.Content.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Content.Location = new System.Drawing.Point(200, 100);
+            this.Content.Name = "Content";
+            this.Content.Size = new System.Drawing.Size(1200, 800);
+            this.Content.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 900);
+            this.Controls.Add(this.Content);
+            this.Controls.Add(this.SideBar);
+            this.Controls.Add(this.TopMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopMenu;
+        private System.Windows.Forms.Panel SideBar;
+        private System.Windows.Forms.Panel Content;
+    }
+}
+
